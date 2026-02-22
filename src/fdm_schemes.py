1889a088bf2063c843799cdd13e8d3905db39122
BenVigos <kvig03@gmail.com>
2026-02-22 14:50:34 +0100

    Add wave equation grid convergence analysis

--- src/fdm_schemes.py
+++ src/fdm_schemes.py
@@ -1,9 +1,9 @@
-from scipy import sparse
-import numpy as np
+import warnings
 import numpy as np
 from scipy import sparse
 
-def wave_equation_1d(u0, c, dx, dt, t_max, bcs=(0, 0)):
+
+def wave_equation_1d(u0, c, dx, dt, t_max, bcs=(0, 0), return_dt=False):
     """
     Solves the 1D wave equation using an explicit finite difference scheme.
     The wave equation is: u_tt = c^2 * u_xx
@@ -13,19 +13,19 @@ def wave_equation_1d(u0, c, dx, dt, t_max, bcs=(0, 0)):
     :param dt: tiem step size.
     :param t_max: maximum simulation time.
     :param bcs: boundary conditions (Dirichlet values at left and right ends).
+    :param return_dt: return time step.
     :return: matrix of shape (nt, nx) with the solution at each time step.
     """
-    nx = len(u0)
-    nt = int(t_max / dt)
 
-    # Stability condition (important!)
-    # if c * dt / dx > 1:
-    #     raise ValueError("CFL condition violated: c*dt/dx must be <= 1")
+    nx = len(u0)
+
+    nt = int(np.floor(t_max / dt)) + 1
+
 
     A = laplacian_1d_matrix(nx, dx)
     b = boundary_term(nx, dx, bcs)
 
-    u = np.zeros((nt, nx))
+    u = np.zeros((nt, nx), dtype=float)
 
     # Initial condition
     u[0, :] = u0
@@ -36,8 +36,7 @@ def wave_equation_1d(u0, c, dx, dt, t_max, bcs=(0, 0)):
         + 0.5 * (c * dt) ** 2 * (A @ u[0, 1:-1] + b)
     )
 
-    # Enforce boundary conditions
-    u[:, 0]  = bcs[0]
+    u[:, 0] = bcs[0]
     u[:, -1] = bcs[1]
 
     # Time stepping
@@ -48,6 +47,10 @@ def wave_equation_1d(u0, c, dx, dt, t_max, bcs=(0, 0)):
             + (c * dt) ** 2 * (A @ u[n, 1:-1] + b)
         )
 
+    t = np.linspace(0.0, dt * (nt - 1), nt)
+
+    if return_dt:
+        return u, dt, t
     return u
 
 
@@ -59,7 +62,7 @@ def laplacian_1d_matrix(nx, dx):
     N = nx - 2
 
     A = sparse.diags([1, -2, 1], offsets=[-1, 0, 1], shape=(N, N), dtype=float)
-    return A / dx**2
+    return A / dx ** 2
 
 
 def boundary_term(nx, dx, bcs):
